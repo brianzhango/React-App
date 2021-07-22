88bb28fe96bf4a62f52234baf1ac15e6a2f184d7
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-23 00:06:22 +1000

    Booklist app

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
-function Greeting() {
-  return <h4>this is component</h4>;
+// JSX (return single element)
+
+function BookList() {
+  return <section className='booklist'> 
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section >;
 }
 
-// const GreetingT = () => {
-//   return React.createElement('h4', {}, 'this is component');
-// };
+const Book = () =>{
+  return (
+  <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>)
+};
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+const Image = () => <img src = "https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg" alt="" />
+
+const Title = () => <h1>The Secrets We Keep: A gripping emotional page turner</h1>
+
+const Author = () => <h4 style={{ color: '#617d98', fontSize: '1rem', marginTop: '0.25rem' }}>
+    Kate Hewitt
+  </h4>
+
+ReactDom.render(<BookList />, document.getElementById('root'));
