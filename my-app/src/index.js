6692d93782e640beebd940b553f063c0771dee43
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-24 22:58:23 +1000

    Children props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,6 +6,11 @@ import './index.css';
 
 // setup vars
 const firstBook = {
+  title: 'The Secrets We Keep: A gripping emotional page turner',
+  author:'Kate Hewitt',
+  image: 'https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg',
+}
+const secondBook = {
   title: 'When We Believed in Mermaids: A Novel',
   author:'Barbara ONeal',
   image: 'https://m.media-amazon.com/images/I/81FJvwYyqgL._AC_UL640_FMwebp_QL65_.jpg',
@@ -13,17 +18,26 @@ const firstBook = {
 
 function BookList() {
   return <section className='booklist'> 
-    <Book title = 'The Secrets We Keep: A gripping emotional page turner' author = 'Kate Hewitt' image = 'https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg'/>
-    <Book title = {firstBook.title} author = {firstBook.title} image = {firstBook.image}/>
+    <Book title = {firstBook.title} author = {firstBook.title} image = {firstBook.image}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sapiente, sed ipsa cumque quas veniam consequatur culpa modi autem labore, in hic, explicabo natus eum quos perspiciatis nobis quaerat corrupti!
+      </p>
+    </Book>
+    <Book title = {secondBook.title} author = {secondBook.title} image = {secondBook.image}/>
   </section >;
 }
 
-const Book = (props) =>{
+// JS props destructuring
+
+// children
+const Book = ({image, title, author, children}) =>{
+  //const {image, title, author} = props;
   return (
   <article className='book'>
-    <img src = {props.image} alt="" />
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>
+    <img src = {image} alt="" />
+    <h1>{title}</h1>
+    <h4>{author}</h4>
+    {children}
   </article>)
 };
 
