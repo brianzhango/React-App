df442e84b1c5e667a90fbe783983d01d14bbbc4c
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-24 23:28:46 +1000

    Book info list

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,39 +5,45 @@ import './index.css';
 // JSX (return single element)
 
 // setup vars
-const firstBook = {
-  title: 'The Secrets We Keep: A gripping emotional page turner',
-  author:'Kate Hewitt',
-  image: 'https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg',
-}
-const secondBook = {
-  title: 'When We Believed in Mermaids: A Novel',
-  author:'Barbara ONeal',
-  image: 'https://m.media-amazon.com/images/I/81FJvwYyqgL._AC_UL640_FMwebp_QL65_.jpg',
-}
+const books = [
+
+  {
+    title: 'The Secrets We Keep: A gripping emotional page turner',
+    author:'Kate Hewitt',
+    image: 'https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg',
+  },
+  {
+    title: 'When We Believed in Mermaids: A Novel',
+    author:'Barbara ONeal',
+    image: 'https://m.media-amazon.com/images/I/81FJvwYyqgL._AC_UL640_FMwebp_QL65_.jpg',
+  },
+  {
+    title: 'Broken',
+    author:'Don Winslow',
+    image: 'https://m.media-amazon.com/images/I/81AV8ZCXD9L._AC_UL640_FMwebp_QL65_.jpg',
+  },
+];
 
 function BookList() {
   return <section className='booklist'> 
-    <Book title = {firstBook.title} author = {firstBook.title} image = {firstBook.image}>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sapiente, sed ipsa cumque quas veniam consequatur culpa modi autem labore, in hic, explicabo natus eum quos perspiciatis nobis quaerat corrupti!
-      </p>
-    </Book>
-    <Book title = {secondBook.title} author = {secondBook.title} image = {secondBook.image}/>
+    {books.map((book) => {
+     // const {image, title, author} = book;
+      return (
+      <Book book={book}></Book>
+      );
+    })}
   </section >;
 }
 
 // JS props destructuring
 
-// children
-const Book = ({image, title, author, children}) =>{
-  //const {image, title, author} = props;
+const Book = (props) =>{
+  const {image, title, author} = props.book;
   return (
   <article className='book'>
     <img src = {image} alt="" />
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
   </article>)
 };
 
