86655cb4488f9123739989ba6c3c96ff8fcc95aa
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-24 23:35:54 +1000

    Unique id

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,17 +7,17 @@ import './index.css';
 // setup vars
 const books = [
 
-  {
+  {id:1,
     title: 'The Secrets We Keep: A gripping emotional page turner',
     author:'Kate Hewitt',
     image: 'https://m.media-amazon.com/images/I/915IPRMVw1L._AC_UL640_FMwebp_QL65_.jpg',
   },
-  {
+  {id:2,
     title: 'When We Believed in Mermaids: A Novel',
     author:'Barbara ONeal',
     image: 'https://m.media-amazon.com/images/I/81FJvwYyqgL._AC_UL640_FMwebp_QL65_.jpg',
   },
-  {
+  {id:3,
     title: 'Broken',
     author:'Don Winslow',
     image: 'https://m.media-amazon.com/images/I/81AV8ZCXD9L._AC_UL640_FMwebp_QL65_.jpg',
@@ -27,9 +27,8 @@ const books = [
 function BookList() {
   return <section className='booklist'> 
     {books.map((book) => {
-     // const {image, title, author} = book;
       return (
-      <Book book={book}></Book>
+      <Book key = {book.id} book={book}></Book>
       );
     })}
   </section >;
