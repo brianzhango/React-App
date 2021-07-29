815a9b5abbf693c3c6b73432805261a529fef7ca
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-30 00:05:09 +1000

    Add Number
    Once click the button, the number will add 1, usState

--- React Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ React Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react';
+// useState - function
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState(0);
+
+  const handleClick = () => {
+    setText(text+1);
+  };
+
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className='btn' onClick={handleClick}>change number</button>
+  </React.Fragment>;
 };
 
 export default UseStateBasics;
