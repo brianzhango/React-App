64b47e1817e0159c4953efd19c6315df1ac82bd4
Yuanrui Zhang <yzha0553@student.monash.edu>
2021-07-30 00:30:01 +1000

    useSate array
    Click button to remove all item, or remove the specific item

--- React Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ React Advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -2,15 +2,19 @@ import React, { useState } from 'react';
 // useState - function
 
 const UseStateBasics = () => {
-  const [text, setText] = useState(0);
+  const [text, setText] = useState('random title');
 
   const handleClick = () => {
-    setText(text+1);
+    if (text === 'random title') {
+      setText('hello world');
+    } else {
+      setText('random title');
+    }
   };
 
   return <React.Fragment>
     <h1>{text}</h1>
-    <button className='btn' onClick={handleClick}>change number</button>
+    <button className='btn' onClick={handleClick}>change title</button>
   </React.Fragment>;
 };
 
